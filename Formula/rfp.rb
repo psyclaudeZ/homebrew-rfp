d2c69ea0eb2fd1919a7eb25b79cb755706632b1b
Bowei Zhang <zbw0517@gmail.com>
2025-08-23 13:11:27 -0700

    release: Udpate formula

--- Formula/rfp.rb
+++ Formula/rfp.rb
@@ -1,14 +1,14 @@
 class Rfp < Formula
   desc "A Rust rewrite of Facebook PathPicker for interactive file selection"
   homepage "https://github.com/psyclaudeZ/rfp"
-  url "https://github.com/psyclaudeZ/rfp/archive/v0.3.0.tar.gz"
-  sha256 "1842a07bf73e7e99935007bbc42ccb1ccb846217fdc8e057fd19a50f1b1d7c84"
+  url "https://github.com/psyclaudeZ/rfp/releases/download/v0.3.1/rfp-universal-apple-darwin.tar.gz"
+  sha256 "979365373c2266aa2b7db3449cd3eff49e4651e56c2c357c353e7966169c7fa2"
   license "MIT"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "rfp-universal" => "rfp"
   end
 
   test do
